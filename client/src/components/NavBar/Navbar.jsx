4327fce17e96527e374be496583d6d79c95c8215
NyangeriBonuke1960 <stevennyangeri@gmail.com>
2025-03-06 02:13:16 +0300

    Client: Navbar foundation

--- client/src/components/NavBar/Navbar.jsx
+++ client/src/components/NavBar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './navbar.css'
+import { CiSearch } from "react-icons/ci";
+import { VscAccount } from "react-icons/vsc";
+import { VscGitCompare } from "react-icons/vsc";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='search-div'>
+        <input type='text' placeholder='Search' />
+        <CiSearch className='search' />
+      </div>
+      
+      <div className='account-compare-div'>
+        <div className='account-div'>
+          <VscAccount />
+        </div>
+
+        <div className='compare-div'>
+          <VscGitCompare />
+        </div>
+      </div>
+    </div>
   )
 }
 
