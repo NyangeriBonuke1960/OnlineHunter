50bcfa4eaa26e002fb1add231d638c4d33a301a9
NyangeriBonuke1960 <stevennyangeri@gmail.com>
2025-03-06 00:24:04 +0300

    Client: Finished header

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1>Online Hunter</h1>
+        <h2>Find it, Hunt it, Rent it.</h2>
+    </div>
+  )
+}
+
+export default Header
